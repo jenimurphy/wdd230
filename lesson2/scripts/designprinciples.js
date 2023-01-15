c93b99ec84be33cff4b256fc35c3a077553c6e86
jenimurphy <jenimurhpy@gmail.com>
2023-01-14 18:37:55 -0700

    initial commit

---
+++ lesson2/scripts/designprinciples.js
@@ -0,0 +1,6 @@
+const d = new Date();
+const lastupdate = document.querySelector("#lastupdate");
+const copyright = document.querySelector("#copyright");
+const year = d.getFullYear();
+lastupdate.innerHTML = `Last Updated: ${new Date().toLocaleString("default",'long')}`
+copyright.innerHTML  = `${new Date().getFullYear()}`;
