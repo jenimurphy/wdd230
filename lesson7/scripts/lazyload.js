7590dd89ef3886411bc7dd3df894dfcee19e1d25
jenimurphy <jenimurhpy@gmail.com>
2023-02-17 00:03:02 -0700

    Lesson7

---
+++ lesson7/scripts/lazyload.js
@@ -0,0 +1,34 @@
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+console.log(imagesToLoad);
+const imgOptions = {
+    threshold: .5,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          loadImages(entry.target);
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
