440c13d52b9ddbec5307aee6b7aa43f4b7e271a9
jenimurphy <jenimurhpy@gmail.com>
2023-02-19 00:04:51 -0700

    Updates218

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -4,7 +4,11 @@ const currentWind = document.getElementById("windspeed").innerHTML;
 console.log(currentWind);
 
 function windChill(tempF, speed){
+    if (tempF <= 50 && speed >3) {
     return (35.74 + 0.6215 * tempF - 35.75 * speed ** 0.16 + 0.4275 * tempF * speed ** 0.16).toFixed(2)+ " °F";
+    } else {
+        return "N/A"
+    }
 }
 
 //Call the windChill function
